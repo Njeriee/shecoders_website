1d0d66a9d78a039bbc5350262b5f7f90bfc881c9
sav <savelynnwachira@gmail.com>
2022-10-06 17:44:49 +0300

    footer partly styled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:["./src/**/*.{html,js}"],
+  content:["home.html","./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       backgroundImage:{
@@ -8,5 +8,7 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
