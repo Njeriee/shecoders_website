97bdaaaf8b661e85a3d4e4505be634cb42e98e51
sav <savelynnwachira@gmail.com>
2022-10-16 10:30:47 +0300

    about and coderafiki pages html strucure added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:["home.html","./node_modules/flowbite/**/*.js"],
+  content:["home.html","./src/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       backgroundImage:{
